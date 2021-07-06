5050dda492f143cb79f50b42bdff786682a9f041
ybb <1094307157@qq.com>
2021-07-06 14:23:07 +0000

    Initialize style

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticController@home')->name('name');
+
+Route::get('user', 'UserController@index')->name('user');
