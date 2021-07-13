90bba8fdae69d377ea1208f5c72f99d0bf567dd0
ybb <1094307157@qq.com>
2021-07-13 12:59:31 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
     public static function boot()
     {
         parent::boot();
-        static::creating(function($user){
+        static::creating(function ($user) {
             $user->activation_token = Str::random(10);
         });
     }
@@ -65,5 +65,38 @@ class User extends Authenticatable
     public function getStatuses($orderby = 'desc', $number = 10)
     {
         return $this->statuses()->orderBy('created_at', $orderby)->paginate($number);
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contain($user_id);
     }
 }
